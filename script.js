23e154b9ffa780e5c1e017bc376749688f655545
gustavo9034-arch <gustavo.9034@colegioprogresso.g12.br>
2026-02-05 22:35:46 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function abrir(link) {
-  document.getElementById("frame").src = link;
-}
-
-const video = document.getElementById("videoTopo");
-
-video.addEventListener("click", () => {
-  video.requestFullscreen();
-});
