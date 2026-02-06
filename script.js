c52b2ca57c70dbd2b7f354e49851309dc53d1fff
gustavo9034-arch <gustavo.9034@colegioprogresso.g12.br>
2026-02-05 22:35:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,5 @@ function abrir(link) {
 const video = document.getElementById("videoTopo");
 
 video.addEventListener("click", () => {
-  if (video.requestFullscreen) {
-    video.requestFullscreen();
-  } else if (video.webkitRequestFullscreen) {
-    video.webkitRequestFullscreen();
-  } else if (video.msRequestFullscreen) {
-    video.msRequestFullscreen();
-  }
+  video.requestFullscreen();
 });
